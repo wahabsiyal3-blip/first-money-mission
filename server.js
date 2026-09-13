c742ec633b7801a8291366aeca47701cc41fa1f6
wahabsiyal3-blip <wahabsiyal3@gmail.com>
2026-09-13 05:09:21 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express();
 app.use(express.json({limit:"1mb"}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(__dirname));
 
 app.get("/api/config",(req,res)=>{
   res.json({
